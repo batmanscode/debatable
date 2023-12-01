ae96452a964258a4aaa69dae3d8ef61fdc8f3b63
batmanscode <29989939+batmanscode@users.noreply.github.com>
2023-12-01 20:04:36 +0530

    moved from notebooks to streamlit app (#1)
    - readme
    - devcontainer
    - app

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # debatable
 LLM to address sales objections
+
+---
+
+**Using Codespaces/VSCode**
+
+Everything you need will be installed when you open Codespaces/VSCode; specified in `.devcontainer/`
+
+*Notes for Codespaces:*
+- Jupyter (will be added to the devcontainer later, not used now)
+  - Currently Jupyter notebook doesn’t work on Codespaces for an unknown reason, or at least I can’t, so you’ll have to use JupyterLab
+  - Open with `jupyter lab --NotebookApp.allow_origin='*' --NotebookApp.ip='0.0.0.0'`
+  - For more info on using see https://code.visualstudio.com/docs/datascience/notebooks-web
+- Streamlit
+  - `./.devcontainer/streamlit.sh` is the equivalent to running streamlit with `streamlit run app.py --server.enableCORS false --server.enableXsrfProtection false`
+  - Open with `streamlit run app.py --server.enableCORS false --server.enableXsrfProtection false`
+  - For more info see https://discuss.streamlit.io/t/how-to-make-streamlit-run-on-codespaces/24526/6
---
+++ debatable.py
@@ -0,0 +1,73 @@
+from simpleaichat import AIChat
+import ast
+
+
+# agent
+personality="""You're an award winning expert SaaS sales agent.
+Identify and address the sales objections in the emails given to you.
+You always keep things simple and concise"""
+
+ai = AIChat(
+    console=True,
+    save_messages=False,  # with schema I/O, messages are never saved
+    # model="gpt-3.5-turbo-0613",
+    model="gpt-4-1106-preview",
+    params={"temperature": 0.4},
+    system=personality
+)
+
+
+def identify_objections(email):
+    """Identify sales objections from the given email"""
+
+    objections_prompt = f"""Identify sales objections from the following customer email:
+    {email}
+
+    Your identified objections as bullet points:"""
+
+    objections = ai(objections_prompt)
+
+    return objections
+
+
+def generate_responses(email, product_context, objections):
+    """Generate responses to the given email and product context"""
+
+    if product_context is None:
+        product_context = "none"
+
+    full_prompt = f"""Identify sales objections from the following customer email:
+    {email}
+
+    Your identified objections as bullet points:
+    {objections}
+
+    Product info:
+    {product_context}
+
+    For each of the objections, suggest responses to handle them (more than one suggestion is ok but maximum four).
+    Your output MUST ONLY be a dict where each key is an objection and it's value is a list with all the suggestions.
+
+    your output as dict:"""
+
+    suggestions = ai(full_prompt)
+
+    return suggestions
+
+
+def complete_suggestions(email, product_context):
+    """
+    combine the two functions above, `identify_objections` and `generate_responses`
+    then format the output string into a dict
+    """
+
+    # identify objections
+    objections = identify_objections(email)
+
+    # generate responses
+    suggestions = generate_responses(email, product_context, objections=objections)
+
+    # format the output string into a dict
+    suggestions = ast.literal_eval(suggestions)
+
+    return suggestions
