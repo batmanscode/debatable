21665de062a67d9f919486fbcc413b79537fd3f5
batmanscode <29989939+batmanscode@users.noreply.github.com>
2023-12-26 02:20:33 +0530

    create fastapi wrapper (#10)
    * add aider ai pair programmer
    
    * aider: Created FastAPI wrapper around `complete_suggestions` func in `debatable.py`.
    
    # Aider chat conversation:
    
    USER: aider api.py
    
    ASSISTANT: It seems you want to create a new file named `api.py`. Please provide the specifications or the content you would like to have in this new file, and I will generate the necessary code and the diff for you.
    
    USER: api.py should be a fastapi wrapper around `complete_suggestions` func in `debatable.py`
    
    ASSISTANT: To create the `api.py` file as a FastAPI wrapper around the `complete_suggestions` function in `debatable.py`, I will provide you with the necessary code and the diff.
    
    ```diff
    --- /dev/null
    +++ api.py
    @@ -0,0 +1,20 @@
    +from fastapi import FastAPI, HTTPException
    +from pydantic import BaseModel
    +from debatable import complete_suggestions
    +
    +app = FastAPI()
    +
    +class EmailContext(BaseModel):
    +    email: str
    +    product_context: str
    +
    +@app.post("/complete-suggestions/")
    +def get_complete_suggestions(email_context: EmailContext):
    +    try:
    +        suggestions = complete_suggestions(email_context.email, email_context.product_context)
    +        return suggestions
    +    except Exception as e:
    +        raise HTTPException(status_code=500, detail=str(e))
    +
    +# Note: You will need to add 'fastapi' and 'pydantic' to your requirements.txt
    +# and run the FastAPI server to use this API.
    ```
    
    This diff represents the entire content of the new `api.py` file. It defines a FastAPI application with a single POST endpoint that takes an email and product context as input and returns the complete suggestions using the `complete_suggestions` function from `debatable.py`. The `EmailContext` class is a Pydantic model that is used for request validation.
    
    Please make sure to add `fastapi` and `pydantic` to your `requirements.txt` file and run the FastAPI server to use this API.
    
    * aider: Add fastapi and pydantic to requirements.txt.
    
    # Aider chat conversation:
    
    USER: I added these *read-write* files: requirements.txt
    
    ASSISTANT: To ensure that the `api.py` file can function correctly, we need to add the `fastapi` and `pydantic` libraries to the `requirements.txt` file. Here is the diff that adds these libraries:
    
    ```diff
    --- requirements.txt
    +++ requirements.txt
    @@ ... @@
     streamlit
     openai
     simpleaichat
     ulid-py
     deta
     aider-chat
    +fastapi
    +pydantic
    ```
    
    This diff adds the `fastapi` and `pydantic` libraries to the existing `requirements.txt` file. Make sure to install these dependencies in your environment to use the FastAPI application.
    
    * create fastapi wrapper
    
    - Add API endpoint for suggestions and update API documentation
    - update readme
    
    * Update API and app descriptions
    
    - incl api link in streamlit app
    - explicitly say it's a prototype in api docs
    
    * Update API URL with correct spelling of debatable

---
+++ api.py
@@ -0,0 +1,97 @@
+# Run the API server with: uvicorn api:app --reload
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from debatable import complete_suggestions
+
+
+description = """ ## This is a prototype API for Debatable. It is not intended for production use.
+args:
+- **email**: The email you want to get suggestions for.
+
+- **product_context**: The product context you want to get suggestions for.
+
+<details>
+<summary>Example request in javascript</summary>
+```javascript
+var myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
+
+var raw = JSON.stringify({
+    "email": "",
+    "product_context": "",
+    });
+
+var requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    body: raw,
+    redirect: 'follow'
+};
+
+fetch("http://<your-api-url>/suggest/", requestOptions)
+    .then(response => response.json())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
+```
+</details>
+
+<details>
+<summary>Example request in curl</summary>
+```bash
+curl --location --request POST 'http://<your-api-url>/suggest/' \
+--header 'Content-Type: application/json' \
+--data-raw '{
+    "email": "",
+    "product_context": ""
+}'
+```
+</details>
+
+<details>
+<summary>Example request in python</summary>
+```python
+import requests
+import json
+
+url = "http://<your-api-url>/suggest/"
+
+payload = json.dumps({
+    "email": "",
+    "product_context": ""
+    })
+
+headers = {
+    'Content-Type': 'application/json'
+    }
+
+response = requests.request("POST", url, headers=headers, data=payload)
+
+print(response.text)
+```
+</details>
+"""
+
+app = FastAPI(
+    title="Debatable API",
+    description=description,
+    version="0.1.0",
+)
+
+@app.get("/")
+def root():
+    return "welcome to the debatable api! :) Add /docs to the URL to see Swagger docs or /redoc to see ReDoc docs."
+
+class EmailContext(BaseModel):
+    email: str = "I don't have this problem right now"
+    product_context: str = "An AI tool that suggests responses to sales objections in emails\n- free trials allowed\n- 14 day refunds\n- accepting partnerships on a case by case basis"
+
+@app.post("/suggest/")
+def get_suggestions(email_context: EmailContext):
+    try:
+        suggestions = complete_suggestions(email_context.email, email_context.product_context)
+        return suggestions
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e)) from e
+
+# Note: You will need to add 'fastapi' and 'pydantic' to your requirements.txt
+# and run the FastAPI server to use this API.
